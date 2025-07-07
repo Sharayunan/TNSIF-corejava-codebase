6d324d76b2f55774387dc029a2a9a18ca2630aa5
pravi <pravi@Sharayu>
2025-07-07 21:38:23 +0530

    commit

---
+++ DaySeven/src/Overriding/HDFC.java
@@ -0,0 +1,11 @@
+package Overriding;
+
+public class HDFC extends RBI {
+	
+	public float getRateofInterest()
+	{
+		System.out.println("Base rate of interest of home loan");
+		return 7.5f;
+	}
+
+}
